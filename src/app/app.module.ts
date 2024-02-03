50b9e7e9ef8d20e38e9956bb0b5957fb025e446c
brice150 <brice.lecomte0@gmail.com>
2024-02-03 08:55:31 +0100

    app router renamed

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,7 +3,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { HttpClientModule } from '@angular/common/http';
-import { appRouter } from './app.router';
+import { appRouter } from './app.routes';
 import { UserModule } from './user/user.module';
 import { ObjectsModule } from './objects/objects.module';
 import { MessagesModule } from './messages/messages.module';
